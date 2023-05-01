f25724bb149b85d01a4b21178d4aa01f1ae9cdc9
liuyibo <1299502716@qq.com>
2023-05-01 13:39:08 +0800

    修改了main.py文件

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 # -*- coding: utf-8 -*-
+'''
+Author: liuyibo 1299502716@qq.com
+Date: 2023-04-29 21:35:29
+LastEditors: liuyibo 1299502716@qq.com
+LastEditTime: 2023-05-01 13:38:19
+FilePath: \Gateway_System\main.py
+Description: 主函数，通过python main.py启动网关软件
+'''
+
 
 from multiprocessing import Process
-from test import *
-import logging
+from web.app import create_app
+from control.src import init_device
 
-if __name__ == "__main__":
-    logging.info("app start...")
+'''
+description: 进程：运行网关设备管理APP
+'''
+def create_web():
+    # create flask app
+    web_app = create_app("develop")
+
+    # execute app
+    web_app.run(host="localhost", port="1234", debug=False)
     
+'''
+description: 进程：运行网关设备控制APP
+'''
+def create_control():
+    # init device
+    init_device()
+    
+if __name__ == "__main__":
     # 创建driver进程
     proc_control = Process(target=create_control, args=())
     proc_control.start()
@@ -17,5 +41,3 @@ if __name__ == "__main__":
     
     proc_control.join()
     proc_web.join()
-
-    logging.critical("app end...")
