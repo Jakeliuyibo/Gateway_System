c698b8a0177b6fc8de3d94afc5fdc8322e7f4448
liuyibo <1299502716@qq.com>
2023-05-20 15:51:46 +0800

    修改了格式

--- main.py
+++ main.py
@@ -3,10 +3,11 @@
 Author: liuyibo 1299502716@qq.com
 Date: 2023-04-29 21:35:29
 LastEditors: liuyibo 1299502716@qq.com
-LastEditTime: 2023-05-15 10:01:16
+LastEditTime: 2023-05-20 13:07:17
 FilePath: \Gateway_System\main.py
 Description: 主函数，通过python main.py启动网关软件
 '''
+# 为了防止Linux使用sudo python main.py执行程序时与python main.py的调用包不同，因此添加路径
 # import sys
 # sys.path.insert(0, r"/home/nano/.local/lib/python3.8/site-packages")
 from multiprocessing import Process
@@ -35,7 +36,7 @@ if __name__ == "__main__":
     proc_control = Process(target=create_control, args=())
     proc_control.start()
 
-    # # # # 创建测试web服务器进程
+    # 创建测试web服务器进程
     proc_web = Process(target=create_web, args=())
     proc_web.start()
 
